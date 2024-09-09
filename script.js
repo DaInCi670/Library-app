bbf93c6e394b62403e3961e5dbb8733c784d357d
Emmanuel Owusu Yeboah <123456789+odin@users.noreply.github.com>
2024-09-09 13:06:50 +0000

    Add functions to add books

--- script.js
+++ script.js
@@ -8,6 +8,66 @@ const totalBookNum = document.querySelector(".library-book-num");
 const overlay = document.querySelector(".overlay");
 const libraryShelf = document.querySelector("main");
 
+const myLibrary = [];
+
+function Book(author, title, pageNumber, isRead) {
+  this.author = author;
+  this.title = title;
+  this.pageNumber = pageNumber;
+  this.isRead = isRead;
+}
+
+Book.prototype.changeReadStatus = function () {
+  return this.isRead ? "Read" : "Not Read";
+};
+
+function addBookToLibarary(a, b, c, d) {
+  const complete = new Book(a, b, c, d);
+  return myLibrary.push(complete);
+}
+
+function displayBook(arr) {
+  libraryShelf.innerHTML = "";
+  return arr.forEach((item, index) => {
+    const bookContainer = document.createElement("div");
+    bookContainer.setAttribute("id", index);
+    const bookTitle = document.createElement("h4");
+    const bookAuthor = document.createElement("p");
+    const bookPageNumber = document.createElement("p");
+    const removeBtn = document.createElement("button");
+    const readStatusBtn = document.createElement("button");
+    bookTitle.textContent = item.title;
+    bookAuthor.textContent = `Author: ${item.author}`;
+    bookPageNumber.textContent = `Total Pages: ${item.pageNumber}`;
+    removeBtn.textContent = "Remove";
+    readStatusBtn.textContent = `${item.changeReadStatus()}`;
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookPageNumber);
+    bookContainer.appendChild(removeBtn);
+    bookContainer.appendChild(readStatusBtn);
+    bookContainer.classList.add("bookContainer");
+    libraryShelf.appendChild(bookContainer);
+
+    readStatusBtn.addEventListener("click", (e) => {
+      e.target.textContent === "Read"
+        ? (e.target.textContent = "Not Read")
+        : (e.target.textContent = "Read");
+    });
+
+    removeBtn.addEventListener("click", (e) => {
+      myLibrary.splice(Number(e.target.parentElement.id), 1);
+      checkBookCount(myLibrary);
+      displayBook(myLibrary);
+    });
+  });
+}
+
+function checkBookCount(Bookarr) {
+  let book = Bookarr.length;
+  totalBookNum.textContent = `Total Books: ${book}`;
+}
+
 closeBtn.addEventListener("click", () => {
   formBox.classList.toggle("toggle-form-box");
   overlay.classList.toggle("hidden");
@@ -18,3 +78,20 @@ addBookBtn[1].addEventListener("click", () => {
   overlay.classList.toggle("hidden");
 });
 
+submitBook.addEventListener("click", (e) => {
+  const title = e.target.form[0].value;
+  const author = e.target.form[1].value;
+  const pages = e.target.form[2].value;
+  const isRead = e.target.form[3].checked;
+  console.log(pages.length);
+
+  if (!title || !author || pages.length < 1) {
+    return;
+  }
+  e.target.form[0].value = " ";
+  e.target.form[1].value = " ";
+  e.target.form[2].value = " ";
+  addBookToLibarary(author, title, pages, isRead);
+  displayBook(myLibrary);
+  checkBookCount(myLibrary);
+});
